8090d4d4ab370ab98b372fa2a100f28bf23bb308
Aziz <shaks.fon@yandex.ru>
2018-02-11 15:07:17 +0500

    Does it work?

--- src/AppBundle/Controller/GenController.php
+++ src/AppBundle/Controller/GenController.php
@@ -47,7 +47,6 @@ class GenController extends Controller
 	
 	return $this->render('default/qr.html.twig', array(
             'form' => $form->createView(),
-			'dir' => $dir,
         ));
     }
 }
